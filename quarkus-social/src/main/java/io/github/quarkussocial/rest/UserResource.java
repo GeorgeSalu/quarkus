f176313a52feb905d8215fc9467b19ff6aa6e259
GeorgeSalu <george.salu10@gmail.com>
2022-10-28 11:14:08 -0300

    recebendo e devolvendo objetos json

--- quarkus-social/src/main/java/io/github/quarkussocial/rest/UserResource.java
+++ quarkus-social/src/main/java/io/github/quarkussocial/rest/UserResource.java
@@ -1,16 +1,27 @@
 package io.github.quarkussocial.rest;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import io.github.quarkussocial.rest.dto.CreateUserRequest;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
 	@POST
-	public Response createUser(CreateUserRequest userRequest) {
+	public Response createUser(	CreateUserRequest userRequest) {
+		return Response.ok(userRequest).build();
+	}
+	
+	@GET
+	public Response listAllUsers() {
 		return Response.ok().build();
 	}
 	
