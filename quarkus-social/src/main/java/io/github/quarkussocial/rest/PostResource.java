1ab8c18528b1c0b6c2f56f38eff6b9fc43652332
GeorgeSalu <george.salu10@gmail.com>
2022-11-01 15:11:03 -0300

    implementando o metodo de realizar postagens

--- quarkus-social/src/main/java/io/github/quarkussocial/rest/PostResource.java
+++ quarkus-social/src/main/java/io/github/quarkussocial/rest/PostResource.java
@@ -1,21 +1,58 @@
 package io.github.quarkussocial.rest;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import io.github.quarkussocial.domain.model.Post;
+import io.github.quarkussocial.domain.model.User;
+import io.github.quarkussocial.domain.repository.PostRepository;
+import io.github.quarkussocial.domain.repository.UserRepository;
+import io.github.quarkussocial.rest.dto.CreatePostRequest;
 
 @Path("/users/{userId}/posts")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class PostResource {
 
-	public Response savePost() {
+	private UserRepository userRepository;
+	private PostRepository postRepository;
+
+	@Inject
+	public PostResource(UserRepository userRepository, PostRepository postRepository) {
+		this.userRepository = userRepository;
+		this.postRepository = postRepository;
+	}
+	
+	@POST
+	public Response savePost(@PathParam("userId") Long userId, CreatePostRequest request) {
+		User user = userRepository.findById(userId);
+		if(user == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+		
+		Post post = new Post();
+		post.setText(request.getText());
+		post.setUser(user);
+		
+		postRepository.persist(post);
+		
 		return Response.status(Response.Status.CREATED).build();
 	}
 	
-	public Response listPost() {
+	@GET
+	public Response listPost(@PathParam("userId") Long userId) {
+		User user = userRepository.findById(userId);
+		if(user == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+		
 		return Response.ok().build();
 	}
 	
