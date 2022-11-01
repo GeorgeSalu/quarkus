5c3773a227690dd5738655e673109ddff18298ee
GeorgeSalu <george.salu10@gmail.com>
2022-11-01 14:13:19 -0300

    usando lombok

--- quarkus-social/src/main/java/io/github/quarkussocial/rest/dto/FieldError.java
+++ quarkus-social/src/main/java/io/github/quarkussocial/rest/dto/FieldError.java
@@ -1,5 +1,8 @@
 package io.github.quarkussocial.rest.dto;
 
+import lombok.Data;
+
+@Data
 public class FieldError {
 
 	private String field;
@@ -8,22 +11,6 @@ public class FieldError {
 	public FieldError(String field, String message) {
 		super();
 		this.field = field;
-		this.message = message;
-	}
-
-	public String getField() {
-		return field;
-	}
-
-	public void setField(String field) {
-		this.field = field;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
 		this.message = message;
 	}
 
