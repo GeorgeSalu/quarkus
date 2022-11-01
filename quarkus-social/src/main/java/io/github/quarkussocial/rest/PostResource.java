15954d49020a8c29b438dbe847605256915364df
GeorgeSalu <george.salu10@gmail.com>
2022-11-01 14:54:16 -0300

    criando o resource de posts e os endpoints

---
+++ quarkus-social/src/main/java/io/github/quarkussocial/rest/PostResource.java
@@ -0,0 +1,22 @@
+package io.github.quarkussocial.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/users/{userId}/posts")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class PostResource {
+
+	public Response savePost() {
+		return Response.status(Response.Status.CREATED).build();
+	}
+	
+	public Response listPost() {
+		return Response.ok().build();
+	}
+	
+}
