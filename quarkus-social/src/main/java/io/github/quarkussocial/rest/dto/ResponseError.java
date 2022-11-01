2195d9d502660790abc142ee4bdcf77a4ff988cc
GeorgeSalu <george.salu10@gmail.com>
2022-11-01 11:21:17 -0300

    alterando os codigos de status para mas especificos

--- quarkus-social/src/main/java/io/github/quarkussocial/rest/dto/ResponseError.java
+++ quarkus-social/src/main/java/io/github/quarkussocial/rest/dto/ResponseError.java
@@ -6,8 +6,11 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.validation.ConstraintViolation;
+import javax.ws.rs.core.Response;
 
 public class ResponseError {
+	
+	public static final int UNPROCESSABLE_ENTITY_STATUS = 422;
 
 	private String message;
 	private Collection<FieldError> errors;
@@ -46,6 +49,10 @@ public class ResponseError {
 
 	public void setErrors(Collection<FieldError> errors) {
 		this.errors = errors;
+	}
+	
+	public Response withStatusCode(int code) {
+		return Response.status(code).entity(this).build();
 	}
 
 }
