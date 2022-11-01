0a00633b547e1a9955281c9604b071d106f21d4b
GeorgeSalu <george.salu10@gmail.com>
2022-11-01 11:33:23 -0300

    inclui formatação

--- quarkus-social/src/main/java/io/github/quarkussocial/rest/dto/ResponseError.java
+++ quarkus-social/src/main/java/io/github/quarkussocial/rest/dto/ResponseError.java
@@ -9,14 +9,12 @@ import javax.validation.ConstraintViolation;
 import javax.ws.rs.core.Response;
 
 public class ResponseError {
-	
+
 	public static final int UNPROCESSABLE_ENTITY_STATUS = 422;
 
 	private String message;
 	private Collection<FieldError> errors;
 
-	
-	
 	public ResponseError(String message, Collection<FieldError> errors) {
 		super();
 		this.message = message;
@@ -24,17 +22,16 @@ public class ResponseError {
 	}
 
 	public static <T> ResponseError createFromValidation(Set<ConstraintViolation<T>> violations) {
-		List<FieldError> errors = violations
-			.stream()
-			.map(cv -> new FieldError(cv.getPropertyPath().toString(), cv.getMessage()))
-			.collect(Collectors.toList());
-		
+		List<FieldError> errors = violations.stream()
+				.map(cv -> new FieldError(cv.getPropertyPath().toString(), cv.getMessage()))
+				.collect(Collectors.toList());
+
 		String message = "Validation Error";
-		
+
 		var responseError = new ResponseError(message, errors);
 		return responseError;
 	}
-	
+
 	public String getMessage() {
 		return message;
 	}
@@ -50,7 +47,7 @@ public class ResponseError {
 	public void setErrors(Collection<FieldError> errors) {
 		this.errors = errors;
 	}
-	
+
 	public Response withStatusCode(int code) {
 		return Response.status(code).entity(this).build();
 	}
