7b372af49fea893515545cbb6846fc9143f96108
GeorgeSalu <george.salu10@gmail.com>
2022-11-04 10:49:37 -0300

    implementando o metodo de seguir um usuario

--- quarkus-social/src/main/java/io/github/quarkussocial/rest/FollowerResource.java
+++ quarkus-social/src/main/java/io/github/quarkussocial/rest/FollowerResource.java
@@ -1,12 +1,19 @@
 package io.github.quarkussocial.rest;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import io.github.quarkussocial.domain.model.Follower;
+import io.github.quarkussocial.domain.model.User;
 import io.github.quarkussocial.domain.repository.FollowerRepository;
 import io.github.quarkussocial.domain.repository.UserRepository;
+import io.github.quarkussocial.rest.dto.FollowerRequest;
 
 @Path("/users/{userId}/followers")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -14,11 +21,32 @@ import io.github.quarkussocial.domain.repository.UserRepository;
 public class FollowerResource {
 
 	private UserRepository userRepository;
-	private FollowerRepository followerRepository;
+	private FollowerRepository repository;
 
-	public FollowerResource(FollowerRepository followerRepository,UserRepository userRepository) {
-		this.followerRepository = followerRepository;
+	@Inject
+	public FollowerResource(FollowerRepository repository,UserRepository userRepository) {
+		this.repository = repository;
 		this.userRepository = userRepository;
+	}
+	
+	
+	@PUT
+	public Response followerUser(@PathParam("userId") Long userId, FollowerRequest request) {
+		User user = userRepository.findById(userId);
+		
+		if(user == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+		
+		var follower = userRepository.findById(request.getFollowerId());
+		
+		var entity = new Follower();
+		entity.setUser(user);
+		entity.setFollower(follower);
+		
+		repository.persist(entity);
+		
+		return Response.status(Response.Status.NO_CONTENT).build();
 	}
 	
 }
