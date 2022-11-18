580bf213a3561a57f06cbceb3d2596ca3cb64b87
GeorgeSalu <george.salu10@gmail.com>
2022-11-18 13:25:38 -0300

    testando o caso de error de validação

--- quarkus-social/src/test/java/io/github/quarkussocial/rest/UserResourceTest.java
+++ quarkus-social/src/test/java/io/github/quarkussocial/rest/UserResourceTest.java
@@ -4,11 +4,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import io.github.quarkussocial.rest.dto.CreateUserRequest;
+import io.github.quarkussocial.rest.dto.ResponseError;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.Map;
 
 @QuarkusTest
 public class UserResourceTest {
@@ -33,4 +37,24 @@ public class UserResourceTest {
 		assertNotNull(response.jsonPath().getString("id"));
 	}
 	
+	@Test
+	@DisplayName("should return error whene json is not valid")
+	public void createUserValidationErroTest() {
+		var user = new CreateUserRequest();
+		user.setAge(null);
+		user.setName(null);
+		
+		var response = 
+				given()
+					.contentType(ContentType.JSON)
+					.body(user)
+				.when()
+					.post("/users")
+				.then()
+					.extract().response();
+				
+		
+		assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+		assertEquals("Validation Error", response.jsonPath().getString("message"));
+	}
 }
