2c2505e50c8dfbd77196830ee51357fa2c4d3ca1
GeorgeSalu <george.salu10@gmail.com>
2022-11-18 14:09:17 -0300

    realizando testes ordenados

--- quarkus-social/src/test/java/io/github/quarkussocial/rest/UserResourceTest.java
+++ quarkus-social/src/test/java/io/github/quarkussocial/rest/UserResourceTest.java
@@ -1,24 +1,35 @@
 package io.github.quarkussocial.rest;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import io.github.quarkussocial.rest.dto.CreateUserRequest;
 import io.github.quarkussocial.rest.dto.ResponseError;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserResourceTest {
+	
+	@TestHTTPResource("/users")
+	URL apiUrl;
 
 	@Test
 	@DisplayName("should create an user successfully")
+	@Order(1)
 	public void createUserTest() {
 		var user = new CreateUserRequest();
 		user.setName("Fulano");
@@ -39,6 +50,7 @@ public class UserResourceTest {
 	
 	@Test
 	@DisplayName("should return error whene json is not valid")
+	@Order(2)
 	public void createUserValidationErroTest() {
 		var user = new CreateUserRequest();
 		user.setAge(null);
@@ -61,5 +73,19 @@ public class UserResourceTest {
 		
 		assertNotNull(errors.get(0).get("message"));
 		assertNotNull(errors.get(1).get("message"));
+	}
+	
+	@Test
+	@DisplayName("should list all users")
+	@Order(3)
+	public void listAllUsersTest() {
+		given()
+			.contentType(ContentType.JSON)
+		.when()
+			.get(apiUrl)
+		.then()
+			.statusCode(200)
+			.body("size()", Matchers.is(1));
+			
 	}
 }
