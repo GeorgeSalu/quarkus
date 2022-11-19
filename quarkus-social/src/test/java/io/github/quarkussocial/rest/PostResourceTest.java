20c2bf52de46b9ec23f74b2b085e3e237479fa4a
GeorgeSalu <george.salu10@gmail.com>
2022-11-19 17:38:49 -0300

    criando cenario com beforeEach

---
+++ quarkus-social/src/test/java/io/github/quarkussocial/rest/PostResourceTest.java
@@ -0,0 +1,57 @@
+package io.github.quarkussocial.rest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import io.github.quarkussocial.domain.model.User;
+import io.github.quarkussocial.domain.repository.UserRepository;
+import io.github.quarkussocial.rest.dto.CreatePostRequest;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+
+@QuarkusTest
+@TestHTTPEndpoint(PostResource.class)
+public class PostResourceTest {
+
+	@Inject
+	UserRepository userRepository;
+	
+	@BeforeEach
+	@Transactional
+	public void setup() {
+		var user = new User();
+		user.setAge(30);
+		user.setName("fulano");
+		
+		userRepository.persist(user);
+	}
+	
+	@Test
+	@DisplayName("should create a post for a user")
+	public void createPostTest() {
+		var postRequest = new CreatePostRequest();
+		postRequest.setText("Some text");
+		
+		var userID = 1;
+		
+		given()
+			.contentType(ContentType.JSON)
+			.body(postRequest)
+			.pathParam("userId", userID)
+		.when()
+			.post()
+		.then()
+			.statusCode(201);
+		
+	}
+	
+}
