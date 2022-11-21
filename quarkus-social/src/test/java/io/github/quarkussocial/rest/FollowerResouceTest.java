536344520a35494a17fa6a39e62d7f10fcd5e7e2
GeorgeSalu <george.salu10@gmail.com>
2022-11-21 11:26:41 -0300

    testes na api de followers

---
+++ quarkus-social/src/test/java/io/github/quarkussocial/rest/FollowerResouceTest.java
@@ -0,0 +1,73 @@
+package io.github.quarkussocial.rest;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.core.Response;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import io.github.quarkussocial.domain.model.User;
+import io.github.quarkussocial.domain.repository.UserRepository;
+import io.github.quarkussocial.rest.dto.FollowerRequest;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+@TestHTTPEndpoint(FollowerResource.class)
+public class FollowerResouceTest {
+
+	@Inject
+	UserRepository userRepository;
+	Long userId;
+	
+	@BeforeEach
+	@Transactional
+	void setup() {
+		// usuario padrão dos testes
+		var user = new User();
+		user.setAge(30);
+		user.setName("fulano");
+		userRepository.persist(user);
+		userId = user.getId();
+	}
+	
+	@Test
+	@DisplayName("should return 409 when Follower Id is equal to User id")
+	public void sameUserAsFollowerTest() {
+		var body = new FollowerRequest();
+		body.setFollowerId(userId);
+		
+		given()
+			.contentType(ContentType.JSON)
+			.body(body)
+			.pathParam("userId", userId)
+		.when()
+			.put()
+		.then()
+			.statusCode(Response.Status.CONFLICT.getStatusCode());
+	}
+	
+	@Test
+	@DisplayName("should return 404 when user id dosen't exists")
+	public void sameNotFountTest() {
+		var body = new FollowerRequest();
+		body.setFollowerId(userId);
+		
+		var inexistenUserId = 999;
+		
+		given()
+			.contentType(ContentType.JSON)
+			.body(body)
+			.pathParam("userId", inexistenUserId)
+		.when()
+			.put()
+		.then()
+			.statusCode(Response.Status.NOT_FOUND.getStatusCode());
+	}
+	
+}
