cbeeb508eeaf2cdc9dc159edb40a2c7626ef7ef0
GeorgeSalu <george.salu10@gmail.com>
2023-11-04 12:02:04 -0300

    removendo a dependencia resteasy-jsonb quebrando o build ainda precisa de investigacao

---
+++ quarkus-social/src/main/java/org/acme/rest/UserResource.java
@@ -0,0 +1,17 @@
+package org.acme.rest;
+
+import org.acme.dto.CreateUserRequest;
+
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+@Path("/users")
+public class UserResource {
+
+	@POST
+	public Response createUser( CreateUserRequest userRequest ) {
+		return Response.ok().build();
+	}
+	
+}
