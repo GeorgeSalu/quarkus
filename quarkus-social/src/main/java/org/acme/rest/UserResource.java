778eb78905a658d9b3971787a59675259272faf6
GeorgeSalu <george.salu10@gmail.com>
2023-11-04 12:16:30 -0300

    recebendo e devolvendo objetos json

--- quarkus-social/src/main/java/org/acme/rest/UserResource.java
+++ quarkus-social/src/main/java/org/acme/rest/UserResource.java
@@ -2,16 +2,21 @@ package org.acme.rest;
 
 import org.acme.dto.CreateUserRequest;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
 	@POST
 	public Response createUser( CreateUserRequest userRequest ) {
-		return Response.ok().build();
+		return Response.ok(userRequest).build();
 	}
 	
 }
