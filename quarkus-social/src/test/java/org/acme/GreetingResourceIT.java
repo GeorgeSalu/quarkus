97ce1cac1ab42ddf87bfec56a4c6a21ece311a3f
GeorgeSalu <george.salu10@gmail.com>
2023-11-04 12:21:49 -0300

    removendo GreetingResourceTest criando na geracao do projeto

--- quarkus-social/src/test/java/org/acme/GreetingResourceIT.java
+++ quarkus-social/src/test/java/org/acme/GreetingResourceIT.java
@@ -3,6 +3,7 @@ package org.acme;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
+//class GreetingResourceIT extends GreetingResourceTest {
+class GreetingResourceIT {
     // Execute the same tests but in packaged mode.
 }
