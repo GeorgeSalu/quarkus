dc8e631a4bba736ef168078bbef45d7fdc8d5381
GeorgeSalu <george.salu10@gmail.com>
2023-11-07 12:14:48 -0300

    concluindo as operacoes de curd

--- quarkus-social/src/main/java/org/acme/rest/UserResource.java
+++ quarkus-social/src/main/java/org/acme/rest/UserResource.java
@@ -3,12 +3,16 @@ package org.acme.rest;
 import org.acme.dto.CreateUserRequest;
 import org.acme.model.User;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -34,6 +38,31 @@ public class UserResource {
 	public Response listAllUsers() {
 		PanacheQuery<User> query = User.findAll();
 		return Response.ok(query.list()).build();
+	}
+	
+	@DELETE
+	@Path("{id}")
+	@Transactional
+	public Response deleteuser(@PathParam("id") Long id) {
+		User user = User.findById(id);
+		if(user != null) {
+			user.delete();
+			return Response.ok().build();
+		}
+		return Response.status(Response.Status.NOT_FOUND).build();
+	}
+	
+	@PUT
+	@Path("{id}")
+	@Transactional
+	public Response updateUser(@PathParam("id") Long id, CreateUserRequest userData) {
+		User user = User.findById(id);
+		if(user != null) {
+			user.setName(userData.getName());
+			user.setAge(userData.getAge());
+			return Response.ok().build();
+		}
+		return Response.status(Response.Status.NOT_FOUND).build();
 	}
 	
 }
