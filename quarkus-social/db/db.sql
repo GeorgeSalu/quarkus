213d3d1a9849627ab6a54d3bc7b4279d311f6e95
GeorgeSalu <george.salu10@gmail.com>
2023-11-08 19:28:06 -0300

    corrige db.sql

--- quarkus-social/db/db.sql
+++ quarkus-social/db/db.sql
@@ -1,6 +1,6 @@
-CREATE TABLE quarkus-social;
+create database quarkus-social;
 
-CREATE TABLE USERS (
+create table USERS (
 	id bigserial not null primary key,
 	name varchar(100) not null,
 	age integer not null
