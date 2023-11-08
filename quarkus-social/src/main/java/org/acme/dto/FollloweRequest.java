95dce6e80245a24f7d43fe300c7851bf8bc9cebb
GeorgeSalu <george.salu10@gmail.com>
2023-11-08 20:29:03 -0300

    implementando o metodo de seguir um usuario

---
+++ quarkus-social/src/main/java/org/acme/dto/FollloweRequest.java
@@ -0,0 +1,15 @@
+package org.acme.dto;
+
+public class FollloweRequest {
+
+	private Long followerId;
+
+	public Long getFollowerId() {
+		return followerId;
+	}
+
+	public void setFollowerId(Long followerId) {
+		this.followerId = followerId;
+	}
+
+}
--- quarkus-social/src/main/java/org/acme/rest/FollowerResource.java
+++ quarkus-social/src/main/java/org/acme/rest/FollowerResource.java
@@ -1,13 +1,18 @@
 package org.acme.rest;
 
+import org.acme.dto.FollloweRequest;
+import org.acme.model.Follower;
 import org.acme.repository.FollowerRepository;
 import org.acme.repository.UserRepository;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/users/{userId}/followers")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -21,6 +26,24 @@ public class FollowerResource {
 	public FollowerResource(FollowerRepository repository,UserRepository userRepository) {
 		this.repository = repository;
 		this.userRepository = userRepository;
+	}
+	
+	@PUT
+	public Response followerUser(@PathParam("userId") Long userId,FollloweRequest request) {
+		var user = userRepository.findById(userId);
+		if(user == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+		
+		var follower = userRepository.findById(request.getFollowerId());
+		
+		var entity = new Follower();
+		entity.setUser(user);
+		entity.setFollower(follower);
+		
+		repository.persist(entity);
+		
+		return Response.status(Response.Status.NO_CONTENT).build();
 	}
 	
 }
