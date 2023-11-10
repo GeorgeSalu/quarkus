26d07c667b65a04c128c3bb3f13b1780b88bf72c
GeorgeSalu <george.salu10@gmail.com>
2023-11-09 21:24:20 -0300

    testando caso de erro de validacao

--- quarkus-social/src/test/java/org/acme/UserResourceTest.java
+++ quarkus-social/src/test/java/org/acme/UserResourceTest.java
@@ -5,8 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 
 import org.acme.dto.CreateUserRequest;
+import org.acme.dto.ResponseError;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -38,6 +41,30 @@ public class UserResourceTest {
 
         assertEquals(201, response.statusCode());
         assertNotNull(response.jsonPath().getString("id"));
+
+    }
+	
+    @Test
+    @DisplayName("should return error when json is not valid")
+    public void createUserValidationErrorTest(){
+        var user = new CreateUserRequest();
+        user.setAge(null);
+        user.setName(null);
+
+        var response = given()
+                        .contentType(ContentType.JSON)
+                        .body(user)
+                .when()
+                    .post(apiURL)
+                .then()
+                    .extract().response();
+
+        assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+        assertEquals("Validation Error", response.jsonPath().getString("message"));
+
+        List<Map<String, String>> errors = response.jsonPath().getList("errors");
+        assertNotNull(errors.get(0).get("message"));
+        assertNotNull(errors.get(1).get("message"));
 
     }
 	
