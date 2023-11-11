59f5c57df715b2fcb18d40963037db95e63a83dc
GeorgeSalu <george.salu10@gmail.com>
2023-11-11 10:55:21 -0300

    projeto basico - endpoint put

--- basico/src/main/java/org/acme/ProdutoResource.java
+++ basico/src/main/java/org/acme/ProdutoResource.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 
@@ -26,6 +27,13 @@ public class ProdutoResource {
 	@POST
 	@Path("/nome")
 	public String nomeProduto(String nomeProduto) {
+		LocalDateTime dateTime = LocalDateTime.now();
+		return nomeProduto+" foi processado em : "+dateTime;
+	}
+	
+	@PUT
+	@Path("/nome")
+	public String putNomeProduto(String nomeProduto) {
 		LocalDateTime dateTime = LocalDateTime.now();
 		return nomeProduto+" foi processado em : "+dateTime;
 	}
