20de5e0631b9513565ccef60b56e6308d2c01eb1
GeorgeSalu <george.salu10@gmail.com>
2023-11-11 10:31:24 -0300

    projeto basico - endppoint get com parametro

--- basico/src/main/java/org/acme/ProdutoResource.java
+++ basico/src/main/java/org/acme/ProdutoResource.java
@@ -2,14 +2,22 @@ package org.acme;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 
 @Path("/produto")
 public class ProdutoResource {
 
 	@GET
 	@Path("/nome")
-	public String Produto() {
+	public String produto() {
 		return "laptop";
+	}
+	
+	@GET
+	@Path("/nome/{id}")
+	public String produtoById(@PathParam("id") Integer id) {
+		if(id == 1) return "laptop";
+		return "para esse id nao existe produto";
 	}
 	
 }
